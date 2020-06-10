733123bc33d809fc8d2e346d4f990909781d2dbd
Jon <durrjp1@gmail.com>
2020-06-10 12:03:59 -0500

    initial commit journals

---
+++ TabloidCLI/Repositories/JournalRepository.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Collections.Generic;
+using Microsoft.Data.SqlClient;
+using TabloidCLI.Models;
+using TabloidCLI.Repositories;
+
+namespace TabloidCLI
+{
+    public class JournalRepository: 
+}
