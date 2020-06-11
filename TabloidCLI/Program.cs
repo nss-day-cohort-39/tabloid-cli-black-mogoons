0ddab68de858016b1a7cb190d561b90c70153f2b
Crystal Elsey <hello@crystalelsey.com>
2020-06-11 10:20:20 -0500

    merging master

--- TabloidCLI/Program.cs
+++ TabloidCLI/Program.cs
@@ -1,4 +1,10 @@
 ﻿using TabloidCLI.UserInterfaceManagers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography.X509Certificates;
 
 namespace TabloidCLI
 {
@@ -14,6 +20,34 @@ namespace TabloidCLI
                 // When it returns null, we should exit the program;
                 ui = ui.Execute();
             }
+
+            // Display current Background color 
+            // Console.WriteLine("Default Background Color: {0}", Console.BackgroundColor);
+
+            // Set the Background color to blue 
+            // Console.BackgroundColor = ConsoleColor.Blue;
+
+            // Display current Background color 
+            // Console.WriteLine("Changed Background Color: {0}", Console.BackgroundColor);
+
+            
+
+            
+
+            
+            string colorChosen = Console.ReadLine();
+
+
+        }
+        public static void DisplayColors()
+        {          
+            // Get the list of available colors 
+            ConsoleColor[] consoleColors = (ConsoleColor[])ConsoleColor.GetValues(typeof(ConsoleColor));
+
+            // Display the list of available console colors 
+            Console.WriteLine("Enter your new background color!");
+            foreach (var color in consoleColors)
+                Console.WriteLine(color);
         }
     }
 }
